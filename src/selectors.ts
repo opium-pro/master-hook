c053ce9db34eb6efafcc6ddad22455742dada5d0
Opium Pro <ilove@opium.pro>
2021-08-02 21:47:57 +0200

    add some types

--- src/selectors.ts
+++ src/selectors.ts
@@ -1,11 +1,12 @@
 import { mediators } from './values'
+import { Mediator } from './utils/create-mediator'
 
 
 export function getSelector(storage: string) {
   if (!mediators[storage]) {
     console.error(`You are trying to get a selector of as unexisting storage: '${storage}'`)
   }
-  const mediator: any = mediators[storage]
+  const mediator: Mediator = mediators[storage]
   return mediator?.get
 }
 
